6c2ddf496c0b7f8ba25ddb56209f4926997d889f
rogerthatdev <roger.that.dev@gmail.com>
2024-05-26 23:30:09 -0700

    vertext: basic sample for creating a new tuned model

---
+++ DialogSum_Data/vertex_new_tune.py
@@ -0,0 +1,26 @@
+
+import time
+
+import vertexai
+from vertexai.preview.tuning import sft
+
+# TODO(developer): Update and un-comment below lines
+# project_id = "PROJECT_ID"
+
+vertexai.init(project=project_id, location="us-central1")
+
+
+
+sft_tuning_job = sft.train(
+    source_model="gemini-1.0-pro-002",
+    train_dataset="gs://cloud-samples-data/ai-platform/generative_ai/sft_train_data.jsonl",
+)
+
+# Polling for job completion
+while not sft_tuning_job.has_ended:
+    time.sleep(60)
+    sft_tuning_job.refresh()
+
+print(sft_tuning_job.tuned_model_name)
+print(sft_tuning_job.tuned_model_endpoint_name)
+print(sft_tuning_job.experiment)
